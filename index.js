c0e557a4f7d3a9ff55da03c30a848b0ac674b018
Terrance Smith <terrancesmith@laquishas-air.mynetworksettings.com>
2023-01-15 20:59:29 -0500

    added to questions variable/started to created array of questions

--- index.js
+++ index.js
@@ -2,7 +2,36 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown ");
 // TODO: Create an array of questions for user input
 const questions = [
-    {}
+    {
+        type: "input",
+        name: "title",
+        message: "Please provide a project title"
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Please provide a project title"
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "Please provide a project title"
+    },
+    {
+        type: "input",
+        name: "Usage Information",
+        message: "Please provide a project title"
+    },
+    {
+        type: "input",
+        name: "Contribution Guidelines",
+        message: "Please provide a project title"
+    },
+    {
+        type: "input",
+        name: "Test Instructions",
+        message: "Please provide a project title"
+    },
 ];
 
 // TODO: Create a function to write README file
