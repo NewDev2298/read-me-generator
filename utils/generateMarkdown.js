6c832af3366206633187e3defabb09736a606530
Terrance Smith <terrancesmith@laquishas-air.mynetworksettings.com>
2023-01-15 19:39:43 -0500

    added sections to readme template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,9 +14,23 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
-  ##
+  #
 
+  ## Description
+
+  ## Table of Contents 
   
+  ## Installation 
+  
+  ## Usage
+  
+  ## License
+  
+  ## Contributing 
+  
+  ## Tests 
+  
+  ## Questions
 
 `;
 }
